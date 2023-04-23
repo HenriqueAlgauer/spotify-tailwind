9e0976b4e4fd6bedd147408151fdc98dcbd184ac
henriqueAlgauer <algauer.henrique@gmail.com>
2023-04-22 22:12:51 -0300

    commit dois

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import {Home as HomeIcon, Search, Library } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -5,17 +6,37 @@ export default function Home() {
      <div className='h-screen flex flex-col'>
        <div className='flex flex-1'>
          <aside className='w-72 bg-zinc-950 p-6'>
-           sidebar
-           <nav>
-             <a href="">
+           <div className='flex items-center gap-2'>
+             <div className='w-3 h-3 bg-red-500 rounded-full '></div>
+             <div className='w-3 h-3 bg-yellow-500 rounded-full'></div>
+             <div className='w-3 h-3 bg-green-500 rounded-full'></div>
+           </div>
+           <nav className='space-y-5 mt-10'>
+             <a href="" className='flex items-center gap-2 text-xs font-semibold text-zinc-200'>
+               <HomeIcon/>
                Home
              </a>
-             <a href="">
+             <a href="" className='flex items-center gap-2 text-xs font-semibold text-zinc-200'>
+               <Search/>
                Search
              </a>
-             <a href="">
+             <a href="" className='flex items-center gap-2 text-xs font-semibold text-zinc-200'>
+               <Library/>
                Your Library
              </a>             
+           </nav>
+
+           <nav className='mt-10 pt-10 border-t border-zinc-700 flex flex-col gap-2'>
+              <a href='' className='text-small text-zinc-400'>Playlist 1</a>
+              <a href='' className='text-small text-zinc-400'>Playlist 2</a>
+              <a href='' className='text-small text-zinc-400'>Playlist </a>
+              <a href='' className='text-small text-zinc-400'>Playlist </a>
+              <a href='' className='text-small text-zinc-400'>Playlist </a>
+              <a href='' className='text-small text-zinc-400'>Playlist </a>
+              <a href='' className='text-small text-zinc-400'>Playlist </a>
+              <a href='' className='text-small text-zinc-400'>Playlist </a>
+              <a href='' className='text-small text-zinc-400'>Playlist </a>
+              <a href='' className='text-small text-zinc-400'>Playlist </a>
            </nav>
          </aside>
          <main className='flex-1 p-6'>
